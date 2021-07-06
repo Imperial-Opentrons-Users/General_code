a87419519bc0d36a5d3aa9e84cbc48200196d555
Geoff Baldwin <geoffbaldwin@users.noreply.github.com>
2021-07-06 15:22:27 +0100

    changed reservoir position

--- Serial_Dilution_Interlab_dye.py
+++ Serial_Dilution_Interlab_dye.py
@@ -22,7 +22,7 @@ def run(protocol: protocol_api.ProtocolContext):
 #Labware
     plate = protocol.load_labware('costar96flatbottomtransparent_96_wellplate_200ul', 1)
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
-    reservoir = protocol.load_labware('brooksreservoirplate_12_wellplate_21000ul', 3)
+    reservoir = protocol.load_labware('brooksreservoirplate_12_wellplate_21000ul', 4)
 #pipettes
     p300 = protocol.load_instrument('p300_multi', 'left', tip_racks=[tiprack_1])
     protocol.max_speeds['Z'] = 100
@@ -39,5 +39,5 @@ def run(protocol: protocol_api.ProtocolContext):
     for i in range(9):
         p300.transfer(100,mix_before=(2, 75),plate['A'+str(i+2)], plate['A'+str(i+3)], mix_before=(2, 75), touch_tip=True, blow_out=True, blowout_location='destination well',new_tip='never')
     p300.drop_tip()
-    for line in protocol.commands(): 
+    for line in protocol.commands():
         print(line)
