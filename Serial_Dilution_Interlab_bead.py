dc0523bd87de32255f19d294f4bb432a0ed3e7ef
Geoff Baldwin <geoffbaldwin@users.noreply.github.com>
2021-07-12 13:13:44 +0100

    define source wells for buffer and dye

--- Serial_Dilution_Interlab_bead.py
+++ Serial_Dilution_Interlab_bead.py
@@ -1,6 +1,4 @@
 #Serial Dilution
-#Load Buffer in Column 1 of 12-well reservoir
-#Load Dye in Column 2 of 12-well reservoir
 
 from opentrons import protocol_api
 
@@ -19,6 +17,11 @@ def run(protocol: protocol_api.ProtocolContext):
 #metadata = {'apiLevel': '2.8'}
 #protocol = simulate.get_protocol_api('2.8')
 
+#Set Column Location of Buffer in 12-well reservoir
+#Load Column Location of Dye in 12-well reservoir
+
+    dye_well='A2'
+    buffer_well='A1'
 #Labware
     plate = protocol.load_labware('costar96flatbottomtransparent_96_wellplate_200ul', 1)
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 2)
@@ -29,14 +32,14 @@ def run(protocol: protocol_api.ProtocolContext):
 #commands
 
 #distributing pbs
-    p300.distribute(100, reservoir.wells_by_name()['A1'], plate.rows_by_name()['A'][1:])
+    p300.distribute(100, reservoir.wells_by_name()[buffer_well], plate.rows_by_name()['A'][1:])
 
 #distributing dye
     p300.pick_up_tip()
     for i in range(2):
         p300.transfer(
         100,
-        reservoir['A2'],
+        reservoir[dye_well],
         plate['A'+str(i+1)],
         mix_before=(4, 75),
         mix_after=(1, 75),
